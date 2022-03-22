04e133f17b0276e1845e9d01b9ea0f44b90f6c62
Mark Deane <mddeane@yahoo.com>
2022-03-21 22:42:53 -0500

    all tests green

--- src/main/java/com/revature/Solution.java
+++ src/main/java/com/revature/Solution.java
@@ -1,5 +1,11 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
 
 	/**
@@ -10,8 +16,41 @@ public class Solution {
 	
 	public static int[] solution(int[] data, int n) {
 		
+		Map <Integer, Integer> dataCount = new LinkedHashMap<Integer, Integer>();
 		
-		return null;
-	}
+		for (int i : data) {
+			if (!dataCount.containsKey(i)) {
+				dataCount.put(i,1);
+			} else {
+				int val = dataCount.get(i);
+				int newVal = ++val;
+				dataCount.put(i,newVal);
+			}
+		}
 
+		Set<Integer> dataKeys = dataCount.keySet();
+		List<Integer> checkList = new ArrayList<Integer>();
+		List<Integer> solutionList = new ArrayList<Integer>();
+		
+		for (int key : dataKeys) {
+			int val = dataCount.get(key);
+			System.out.println("val=" + val);
+			System.out.println("n=" + n);
+			if (val <= n) {
+				checkList.add(key);
+			}
+		}
+
+		for (Integer s : checkList) {
+			for (int d : data) {
+				if (d == s) {
+					solutionList.add(d);
+				}
+			}
+		}
+
+		int[] solutionArray = solutionList.stream().mapToInt(sol -> sol).toArray();
+		
+		return solutionArray;
+	}
 }
